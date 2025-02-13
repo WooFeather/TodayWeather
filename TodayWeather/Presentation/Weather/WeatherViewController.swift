70e20c7a138da4530040714b46ef1b9b39fd7840
Woohyun Jo <bluewing1004@gmail.com>
2025-02-13 16:36:55 +0900

    [Setting] 프로젝트 기본세팅 및 폴더링

--- TodayWeather/Presentation/Weather/WeatherViewController.swift
+++ TodayWeather/Presentation/Weather/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  TodayWeather
 //
 //  Created by 조우현 on 2/13/25.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
     }
-
-
 }
-
